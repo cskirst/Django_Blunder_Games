05b5c3df891b85a30f63d98c881e79dbaaa96db0
Derek <derekpalecek@gmail.com>
2017-11-28 20:41:18 -0600

    General updates changing names

--- Interface/admin.py
+++ Interface/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import User
 from .models import HuntCommand
+from .models import Landmarks
+from .models import Game
+
 
 admin.site.register(User)
 admin.site.register(HuntCommand)
+admin.site.register(Landmarks)
+admin.site.register(Game)
 
