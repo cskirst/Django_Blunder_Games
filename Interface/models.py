e8a26aae648218366e97f10eff4ac380a27265e1
Derek <derekpalecek@gmail.com>
2017-12-02 12:18:31 -0600

    Updates to models

--- Interface/models.py
+++ Interface/models.py
@@ -1,28 +1,27 @@
 from django.db import models
 
 class User(models.Model):
-    def __str__(self):
-        return self.name
     name = models.CharField(max_length=40)
     password = models.CharField(max_length=40)
     currentLandmark = models.IntegerField(default=0)
+    game = models.ForeignKey(Game)
+    def __str__(self):
+        return self.name
 
 class Landmarks(models.Model):
     name = models.CharField(max_length=100)
     clue = models.CharField(max_length=1000)
     question = models.CharField(max_length=1000)
     answer = models.CharField(max_length=1000)
+    game = models.ForeignKey(Game)
+    def __str__(self):
+        return self.name
 
 class Game(models.Model):
     isActive = models.BooleanField(default=False)
-    #landmarkList = models.ForeignKey() #NOT SURE WHAT TO PUT HERE
-    #System = models.ForeignKey() #NOT SURE WHAT TO PUT HERE
-    #teams = models.ForeignKey() #NOT SURE WHAT TO PUT HERE?
-'''
-class System(models.Model): #THIS CLASS MIGHT BE REPLACED BY THE ACTUAL DATABASE THUS MIGHT NOT BE NEEDED HERE
-    landmarks = models.ForeignKey() #NOT SURE WHAT TO PUT HERE
-    team = models.ForeignKey() #NOT SURE WHAT TO PUT HERE
-'''
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class HuntCommand(models.Model):
     def __str__(self):
