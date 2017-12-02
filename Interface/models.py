4c001986bb4405d0176c9b58151f921ad9b4cf10
Derek <derekpalecek@gmail.com>
2017-12-02 12:48:02 -0600

    models.py fixed

--- Interface/models.py
+++ Interface/models.py
@@ -5,6 +5,15 @@ class Game(models.Model):
     isActive = models.BooleanField(default=False)
     def __str__(self):
         return self.name
+    def checkIfWin(self, x):
+        #TODO
+    def toggleActive(self):
+        if (self.isActive):
+        ##THE ~FUTURE~  self.stopClock()
+            self.isActive = False
+        else:
+            ##THE ~FUTURE~   self.startClock()
+            self.isActive = True
 
 class User(models.Model):
     name = models.CharField(max_length=40)
@@ -13,6 +22,20 @@ class User(models.Model):
     game = models.ForeignKey(Game)
     def __str__(self):
         return self.name
+    def getName(self):
+        return self.name
+    def getPassword(self):
+        return self.password
+    def get_status(self):
+        # returns current time, location (penalties accounted for in later iteration)
+        # if gm
+        # implement gm total status sprint 2
+        print("Stats for " + self.name)
+        # print("Current time: %d", self.Game.clock)  # TODO: need to fix this line
+        print("You are on landmark " + self.currentLandmark)  # Prints name of landmark
+
+
+
 
 class Landmarks(models.Model):
     name = models.CharField(max_length=100)
@@ -20,8 +43,31 @@ class Landmarks(models.Model):
     question = models.CharField(max_length=1000)
     answer = models.CharField(max_length=1000)
     game = models.ForeignKey(Game)
+    position = models.IntegerField()
     def __str__(self):
         return self.name
+
+    def getName(self):
+        return self.name
+
+    def setClue(self, clue):  # to be called by gamemaker
+        self.clue = clue
+
+    def getClue(self):
+        return self.clue
+
+    def setQuestion(self, question):  # to be called by gamemaker
+        self.question = question
+
+    def getQuestion(self):
+        return self.question
+
+    def setAnswer(self, answer):  # to be called by gamemaker
+        self.answer = answer
+
+    def getAnswer(self):
+        return self.answer
+
 
 class HuntCommand(models.Model):
     def __str__(self):
