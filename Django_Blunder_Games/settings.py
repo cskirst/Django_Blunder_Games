aa098ffdfb7907db0c3b63fa46918e2e1beda63f
cskirst <31906810+cskirst@users.noreply.github.com>
2017-12-07 09:15:34 -0600

    Concurrency enabled

--- Django_Blunder_Games/settings.py
+++ Django_Blunder_Games/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@0um8*ncj2-@0z=l^l)nsuddb8%-oj-n=z-db#a^i^+tzpcn@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
