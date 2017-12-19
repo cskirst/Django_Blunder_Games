17dccbc97823dc4a8da06a60e02fc749454f5244
Derek <derekpalecek@gmail.com>
2017-12-18 18:09:49 -0600

    Get question and concurrent sessions

--- Interface/views.py
+++ Interface/views.py
@@ -10,31 +10,35 @@ from .models import User, HuntCommand, Game, Landmarks
 def index(request):
     return render(request, 'index.html', {"message":""})
 
-def validate(request):
+def sess(request):
     request.session['User'] = request.POST["User"]
     request.session['password'] = request.POST["password"]
+    request.session.modified = True
+    return validate(request)
+
+def validate(request):
     message = "XXX"
     try:
-        u = User.objects.get(name=request.POST["User"]) #POST
+        u = User.objects.get(name=request.session.get('User')) #POST
 
     except User.DoesNotExist:
-        message = "No user named " + request.POST["User"]
+        message = "No user named " + request.session.get('User')
         return render(request, "index.html", {"message": message})
     else:
-        if u.password != request.POST["password"]:
+        if u.password != request.session.get("password"):
             message = "Invalid password"
     try:
        l = Landmarks.objects.filter(game=u.game)#.order_by('position')
        l = l.values()
+       curL = Landmarks.objects.get(position=u.currentLandmark)
     except Landmarks.DoesNotExist:
         message = "No landmarks"
+        curL = None
     if message == "XXX":
-        context = {"User": request.POST["User"], "Games": Game.objects.all(), "curUser": u, "landList":l}
+        context = {"User": request.session.get('User'), "Games": Game.objects.all(), "curUser": u, "landList":l, "currentLm":curL}
         if u.name == 'admin':
             return render(request,"terminal.html",context)
         else:
-            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            print(len(l))
             return render(request, 'user.html', context)
     else:
         return render(request,"index.html",{"message":message})
@@ -59,14 +63,15 @@ def terminal(request):
 def user(request):
     i = Interface.Interface()
     #passw = request.session['password']
-    u = User.objects.get(name=request.session['User'])#username)   request.POST.get("User")
-    user = request.session['User']
+    u = User.objects.get(name=request.session.get('User'))#username)   request.POST.get("User")
+    user = request.session.get('User')
     try:
         l = Landmarks.objects.filter(game=u.game)#.order_by('position')
         l = l.values()
+        curL = Landmarks.objects.get(position=u.currentLandmark)
     except Landmarks.DoesNotExist:
-        print("WHY IS THERE NO LANDMARKS?????????????") #lol...
         l=[]
+        curL = None
     #u = User.objects.get(name=(request.GET.get("User")))
     prefix = request.POST.get('prefix')
     j = 1
@@ -76,5 +81,5 @@ def user(request):
         j += 1
     i.process(strin, u)
     #context = {"User": u}
-    context = {"User": User, "Games": Game.objects.all(), "curUser": u, "landList": l}
+    context = {"User": User, "Games": Game.objects.all(), "curUser": u, "landList": l, "currentLm": curL}
     return render(request, "user.html", context)
